5a4f9496cc2575c9407c49488acfcaa0ed60cc0d
Ahmed Hisham <a7med.hish@gmail.com>
2020-08-27 15:03:31 +0200

    notebook results

--- utils.py
+++ utils.py
@@ -1,18 +1,17 @@
 import numpy as np
 
-def hough_transform(binary_img, theta_step=0.1):
+def hough_transform(binary_img):
     w, h = binary_img.shape
     diag = int(round(np.sqrt(w*w + h*h)))
-    rho = np.linspace(-diag, diag, num= diag*2)
-    theta = np.arange(-np.pi/2, np.pi/2, theta_step)
-    A = np.zeros((len(rho), len(theta)))
+    rho = np.linspace(-diag, diag, num=diag*2).astype(np.float16)
+    theta = np.linspace(-np.pi/2, np.pi/2, num=len(rho)).astype(np.float16)
+    A = np.zeros((len(rho), len(theta))).astype(np.int8)
 
     cos_t = np.cos(theta)
     sin_t = np.sin(theta)
 
     for iy in range(binary_img.shape[1]):
         for ix in range(binary_img.shape[0]):
-            # print(binary_img[iy,ix])
             if binary_img[ix,iy] == 1:
                 for itheta in range(len(theta)):
                     rho_curr = ix*cos_t[itheta] + iy*sin_t[itheta]
@@ -23,12 +22,17 @@ def hough_transform(binary_img, theta_step=0.1):
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    img = np.array([[1,0,0,0,0],
-                    [0,1,0,0,0],
-                    [0,0,1,0,0],
-                    [0,0,0,1,0],
-                    [0,0,0,0,1]]).astype(np.int16)
+    img = np.array([[1,0,0,0,0,0,0],
+                    [0,1,0,0,0,0,0],
+                    [0,0,1,0,0,0,0],
+                    [0,0,0,1,0,0,0],
+                    [0,0,0,0,1,0,0],
+                    [0,0,0,0,0,1,0],
+                    [0,0,0,0,0,0,1]]).astype(np.int16)
     
     A = hough_transform(img)
+    idx_max = np.unravel_index(np.argmax(A), A.shape)
+    print('index of max point: ', idx_max)
     plt.imshow(A, cmap='gray')
+    plt.show()
     
